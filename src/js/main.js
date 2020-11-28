58bf1750a8eec02a6fdedd14f6cf2de175ee7136
G.Sterczewski <sterczewski.dev@protonmail.ch>
2020-11-28 07:54:15 +0100

    [dev] fixed text contrasts, add alt to images, change <img> element to <picture> for serving smaller images on mobile view

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,29 @@
 const articlesCollection = [{
     title : "Discover innovative ways to decorate",
     body : "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-    img : "desktop-image-hero-1.jpg"
+    img : "image-hero-1.jpg",
+    img : {
+        src : "image-hero-1.jpg",
+        alt: "table with four white chairs"
+    }
 },
 {
     title : "We are available all across the globe",
     body : "With stores all over the world, it's easy for you to find furniture for your home or place of business.Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-    img : "desktop-image-hero-2.jpg"
+    
+    img : {
+        src : "image-hero-2.jpg",
+        alt: "three simple chairs: yellow, white,pink"
+    }
 },
 {
     title : "Manufactured with the best materials",
     body : "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office",
-    img : "desktop-image-hero-3.jpg"
+    
+    img : {
+        src : "image-hero-3.jpg",
+        alt: "black chair"
+    }
 }
 ]
 
@@ -25,6 +37,9 @@ const IDCollection = {
     title : 'js-title',
     body : 'js-body',
     img : 'js-img',
+    sourceMobile : "js-source-mobile",
+    sourceDesktop : "js-source-desktop",
+    sourceDefault : "js-source-default",
     nextArrow : 'js-next',
     nextArrowMobile : 'js-next-mobile',
     previousArrow : 'js-previous',
@@ -46,14 +61,26 @@ const Slider = (nodes,articles) => {
     const imgDir = "assets/images"
     const incrementCount = () => count < max ? count++ : count = 0;
     const decrementCount = () => count > 0 ? count-- : count = max;
-    const getArticle = () => articles[count]
-    const createImgSource = title => `${imgDir}/${title}`
+    const getArticle = () => articles[count];
+    
+    const createImgSource = title => ({
+        mobile : `${imgDir}/mobile-${title}`,
+        desktop : `${imgDir}/desktop-${title}`
+        
+    })
 
+    
     const loadArticle = () => {
         const {title,body,img} = getArticle()
+        const { mobile, desktop} = createImgSource(img.src)
+        
         nodes.title.innerText = title 
         nodes.body.innerText = body 
-        nodes.img.src = createImgSource(img)
+        nodes.sourceMobile.srcset= mobile;
+       
+        nodes.sourceDesktop.srcset = desktop;
+        nodes.sourceDefault.src = desktop;
+        nodes.sourceDefault.alt = img.alt;
     }
     const nextArticle = () => {
         incrementCount()
@@ -96,6 +123,7 @@ window.onload = () => {
     const nodes = hookNodes(IDCollection)
     const navigation = Navigation(nodes)
     const slider = Slider(nodes,articlesCollection)
+    slider.loadArticle()
     nodes.hamburger.addEventListener("click",navigation.show)    
     nodes.closeBtn.addEventListener("click",navigation.hide)
     nodes.nextArrow.addEventListener("click",slider.nextArticle)    
